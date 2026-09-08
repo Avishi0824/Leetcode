43a5438f949865864879ed28385debc56c2192b1
Avishi Gupta <avishiiigupta@gmail.com>
2026-09-08 23:34:39 +0530

    Time: 40 ms (28.02%), Space: 14 MB (88.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -17,3 +17,32 @@ public:
         return {};
     }
 };
+
+/*class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        // HashMap: stores number -> index
+        unordered_map<int, int> mp;
+
+        // Traverse through the array
+        for (int i = 0; i < nums.size(); i++) {
+            
+            // Find the number required to make the target
+            int needed = target - nums[i];
+
+            // Check if the required number already exists in the HashMap
+            if (mp.find(needed) != mp.end()) {
+                
+                // If found, return its index and current index
+                return {mp[needed], i};
+            }
+
+            // Store current number and its index in the HashMap
+            mp[nums[i]] = i;
+        }
+
+        // No valid pair found
+        return {};
+    }
+};*/
