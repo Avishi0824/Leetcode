7788b946b52eeaf354b97619f564e2ed2fe9bd42
Avishi Gupta <avishiiigupta@gmail.com>
2026-09-09 22:58:41 +0530

    Time: 5 ms (25.32%), Space: 10 MB (11.42%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+          if (s.length() != t.length()) {
+            return false;
+        }
+        unordered_map<char, int> count;
+        for (int i = 0; i < s.length(); ++i) {
+            count[s[i]]++;
+            count[t[i]]--;
+        }
+    for (auto const& [key, val] : count) {
+            if (val != 0) return false;
+        }
+        return true;
+    }
+};
